6f8dfa223985b98cf088635a97e332a01c77c826
Angel Manuel <anmato1999@gmail.com>
2022-11-19 01:44:48 -0600

    Septimo Commit Vista RegistrarAlumnos

--- resources/views/livewire/alumnos/ralumnos.blade.php
+++ resources/views/livewire/alumnos/ralumnos.blade.php
@@ -1,3 +1,242 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="w-full">
+        <div class="w-full rounded p-8 sm:p-12 mt-4">
+
+            <p class="text-4xl font-sans leading-tight text-center">DATOS DEL ALUMNO</p>
+            <div class="md:flex items-center mt-12">
+                <div class="w-full md:w-1/4 flex flex-col">
+                    <label class="font-sans leading-none">Numero de Control</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Nombre</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6">
+                    <label class="font-sans leading-none">Apellido Paterno</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Apellido Materno</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+            </div>
+            <div class="md:flex items-center mt-8">
+                <div class="w-full md:w-1/4 flex flex-col">
+                    <label class="font-sans leading-none">CURP</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Lugar de Nacimiento</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6">
+                    <label class="font-sans leading-none">Numero de Calle</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Colonia</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+            </div>
+            <div class="md:flex items-center mt-8">
+                <div class="w-full md:w-1/4 flex flex-col">
+                    <label class="font-sans leading-none">Localidad</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Codigo Postal</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6">
+                    <label class="font-sans leading-none">Lugar de Radicacion</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Numero de Seguro Social</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+            </div>
+            <div class="md:flex items-center mt-8">
+                <div class="w-full md:w-1/4 flex flex-col">
+                    <label class="font-sans leading-none">Tipo de Sangre</label>
+                    <select
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black">
+                        <option>A+</option>
+                        <option>A-</option>
+                        <option>B+</option>
+                        <option>AB+</option>
+                        <option>AB-</option>
+                        <option>O+</option>
+                        <option>O-</option>
+                        <option>No conocido</option>
+                    </select>
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Estado Civil</label>
+                    <select
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black">
+                        <option>Soltero(a)</option>
+                        <option>Casado(a)</option>
+                        <option>Divorciado(a)</option>
+                        <option>Viudo(a)</option>
+                    </select>
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6">
+                    <label class="font-sans leading-none">Correo Electronico</label>
+                    <input type="email"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Telefono</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+            </div>
+
+            <div class="md:flex items-center mt-8">
+                <div class="w-full md:w-1/4 flex flex-col">
+                    <label class="font-sans leading-none">Grado</label>
+                    <select
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                    </select>
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Estatus</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black "
+                        value="Activo" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6">
+                    <label class="font-sans leading-none">Promedio</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Año de Finalizacion</label>
+                    <input type="date"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+            </div>
+
+            <div class="mt-20">
+                <p class="text-4xl font-sans leading-tight text-center">DATOS DEL TUTOR</p>
+            </div>
+
+            <div class="my-2 flex sm:flex-row flex-col pt-4">
+                <div class="block">
+                    <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                    </span>
+                    <input placeholder="Buscar"
+                        class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-black border-b block pl-2 sm:mr-16 md:mr-64 lg:mr-0 py-2 w-96 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                </div>
+                <div>
+                    <button
+                        class="mt-0 font-sans leading-none text-white py-3 px-10 ml-8 bg-[#78163B] rounded hover:bg-[#78163B] focus:ring-2 focus:ring-offset-2 focus:ring-[#78163B] focus:outline-none">
+                        Buscar
+                    </button>
+                </div>
+            </div>
+
+            <div class="md:flex items-center mt-8">
+                <div class="w-full md:w-1/4 flex flex-col">
+                    <label class="font-sans leading-none">Nombre</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Apellido Paterno</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6">
+                    <label class="font-sans leading-none">Apellido Materno</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Celular</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+            </div>
+
+            <div class="mt-24">
+                <p class="text-4xl font-sans leading-tight text-center">DATOS DE LA SECUNDARIA</p>
+            </div>
+
+            <div class="my-2 flex sm:flex-row flex-col pt-4">
+                <div class="block">
+                    <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                    </span>
+                    <input placeholder="Buscar"
+                        class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-black border-b block pl-2 sm:mr-16 md:mr-64 lg:mr-0 py-2 w-96 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                </div>
+                <div>
+                    <button
+                        class="mt-0 font-sans leading-none text-white py-3 px-10 ml-8 bg-[#78163B] rounded hover:bg-[#78163B] focus:ring-2 focus:ring-offset-2 focus:ring-[#78163B] focus:outline-none">
+                        Buscar
+                    </button>
+                </div>
+            </div>
+
+            <div class="md:flex items-center mt-8">
+                <div class="w-full md:w-1/4 flex flex-col">
+                    <label class="font-sans leading-none">Nombre</label>
+                    <input type="text"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Clave de Secundaria</label>
+                    <input type="email"
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6">
+                    <label class="font-sans leading-none">Modalidad</label>
+                    <select
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black">
+                        <option>General</option>
+                        <option>Tecnica</option>
+                        <option>Telesecundaria</option>
+                        <option>Online</option>
+                    </select>
+                </div>
+                <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
+                    <label class="font-sans leading-none">Regimen</label>
+                    <select
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black">
+                        <option>Publica</option>
+                        <option>Privada</option>
+                    </select>
+                </div>
+            </div>
+
+
+            <div class="flex items-center justify-center w-full mt-24">
+                <button
+                    class="mt-9 font-sans leading-none text-white py-4 px-10 bg-[#78163B] rounded hover:bg-[#78163B] focus:ring-2 focus:ring-offset-2 focus:ring-[#78163B] focus:outline-none">
+                    Registrar
+                </button>
+            </div>
+
+        </div>
+    </div>
 </div>
