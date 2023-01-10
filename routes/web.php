2e21891fb4b23d8e0a02113b2a9dcc1d342552a6
Raul Acevedo Flores <raulacevedof549@gmail.com>
2023-01-09 22:59:30 -0600

    VigésimoSeptimo Commit Funcionalidad del loggin, pendiente diseño

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/dashboard');
+    return view('auth.login');
 });
 
 Route::middleware([
@@ -35,7 +35,7 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('dashboard', ['userid' => auth()->user()]);
     })->name('dashboard');
 });
 
