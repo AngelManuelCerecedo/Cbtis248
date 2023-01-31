77aadbd6eba7dd7890bb2ba35466987db5ed5e1a
Raul Acevedo Flores <raulacevedof549@gmail.com>
2023-01-31 01:22:08 -0600

    Validaciones finales

--- resources/views/livewire/administrador/eadministrador.blade.php
+++ resources/views/livewire/administrador/eadministrador.blade.php
@@ -128,12 +128,15 @@
                 <div class="w-full md:w-1/4 flex flex-col md:ml-6 md:mt-0 mt-4">
                     <label class="font-sans leading-none">Perfil Abreviado</label>
                     <input type="text" wire:model='PA'
-                    class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                    @error('PA')
+                        <span class="text-red-600">{{ $message }}</span>
+                    @enderror
                 </div>
                 <div class="w-full md:w-1/4 flex flex-col md:ml-6">
                     <label class="font-sans leading-none">Perfil</label>
                     <input type="text" wire:model='P'
-                    class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
+                        class="leading-none text-black p-3 focus:outline-none focus:border-blue-700 mt-4 bg-white border rounded border-black" />
                     @error('P')
                         <span class="text-red-600">{{ $message }}</span>
                     @enderror
